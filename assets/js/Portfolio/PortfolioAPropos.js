6e126fdfaa04a1a8e539cc13a845b9f477f8c679
anselclement <clem-nogare@hotmail.fr>
2019-05-10 11:50:55 +0200

    Modification organisation dossier scss

--- assets/js/Portfolio/PortfolioAPropos.js
+++ assets/js/Portfolio/PortfolioAPropos.js
@@ -6,7 +6,10 @@ export default function PortfolioAPropos(props) {
     return (
         <section className="section">
             <div className="container">
-                <h2 className="title is-1">A Propos</h2>
+                <h2 className="title is-2 has-text-centered">A Propos</h2>
+            </div>
+            <div className="container">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
         </section>
     );
