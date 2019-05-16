88bd0efd40beb334880b7ed5bfc21c355d05cc80
anselclement <clem-nogare@hotmail.fr>
2019-05-16 10:46:23 +0200

    mise en place base dashboard

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/dashboard")
+     */
+    public function dashboard()
+    {
+        return $this->render('dashboard.html.twig', []);
+    }
+}
